a75b0e5c714a3d0ef2675358b6d08cdd8cf9d756
Umesh Chavan <chavanup1990@outlook.com>
2024-08-07 17:11:48 +0530

    Response resuklt

--- JWT_Ref/Models/Response.cs
+++ JWT_Ref/Models/Response.cs
@@ -1,8 +1,9 @@
 ﻿namespace JWT_Ref.Models
 {
-    public class Response
+    public class Response<T> where T : class
     {
         public string? Status { get; set; }
         public string? Message { get; set; }
+        public T Result { get; set; }
     }
 }
